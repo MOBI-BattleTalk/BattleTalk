eda4562d9fdcec34b895e84be8c742d1a03fba46
ooherin <555ohr@naver.com>
2024-01-21 01:00:38 +0900

    📚Chore: 초기 라이브러리 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        red: '#E3534A',
+        blue: '#1590D6',
+        violet: '#7754DA',
+        pink: '#FD8E8E',
+        skyblue: '#7BC0FF',
+        deepRed: '#BE3A32',
+        deepBlue: '#22526E',
+        black: '#000000',
+        textGrey: '#3B3B3B',
+        darkGrey: '#A4A4A4',
+        lightGrey: '#D9D9D9',
+        background: '#F6F6F6',
+        white: '#FFFFFF',
+      },
+    },
+  },
+  plugins: [],
+};
