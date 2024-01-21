11ce676d8bf4918acfb59ab1c067b63834dbc7b2
ooherin <555ohr@naver.com>
2024-01-22 03:13:46 +0900

    Feat : TopButton 생성

---
+++ src/components/TopButton.tsx
@@ -0,0 +1,40 @@
+// @ts-ignore
+import TopArrow from '@/assets/TopArrow.svg?react';
+// @ts-ignore
+import TopArrowHover from '@/assets/TopArrowHover.svg?react';
+import { useState } from 'react';
+
+function TopButton() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const scrollToTop = () => {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={scrollToTop}
+      className={`w-[65px] h-[65px] rounded-full border-[3px] border-violet flex items-center justify-center font-extrabold flex-col text-violet ${
+        isHovered ? 'hover:text-skyblue hover:border-skyblue' : ''
+      }`}
+    >
+      {isHovered ? <TopArrowHover /> : <TopArrow />}
+      <div>TOP</div>
+    </div>
+  );
+}
+
+export default TopButton;
