6d3ebe5da83ff38fb3caa0e078ebb44cdc828557
ooherin <555ohr@naver.com>
2024-01-22 12:17:49 +0900

    Feat : 글자수세기 컴포넌트 생성

---
+++ src/components/CharaterCounter.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface Props {
+  currentNum: number;
+  maxNum: number;
+}
+
+/**
+ * 폼에서 사용하는 글자수 컴포넌트입니다.
+
+ * // 사용 예시:
+ * <CharacterCounter currentNum="12" maxNum="100"></FormCard>
+ */
+const CharacterCounter: React.FC<Props> = ({ currentNum, maxNum }) => {
+  return (
+    <div className="flex gap-[3px] text-gray-600 font-semibold">
+      <span>{currentNum}</span>
+      <span>/</span>
+      <span>{maxNum}</span>
+    </div>
+  );
+};
+
+export default CharacterCounter;
