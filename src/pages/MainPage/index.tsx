5748bca06f3a24a5541a1563d49f1001f5ba66fc
ooherin <555ohr@naver.com>
2024-01-22 15:23:00 +0900

    Feat : NavBar 생성

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -2,9 +2,11 @@ import MainBattleCard from '@/pages/MainPage/components/MainBattleCard.tsx';
 import TopButton from '@/components/TopButton.tsx';
 import NavigateBox from '@/components/NavigateBox.tsx';
 import { useNavigate } from 'react-router-dom';
+import Navbar from '@/pages/MainPage/components/Navbar.tsx';
 
 const MainPage = () => {
   const navigate = useNavigate();
+
   return (
     <div className="flex items-center justify-center mt-[100px] gap-[20px] flex-col">
       <MainBattleCard />
@@ -37,6 +39,7 @@ const MainPage = () => {
           },
         ]}
       />
+      <Navbar />
     </div>
   );
 };
