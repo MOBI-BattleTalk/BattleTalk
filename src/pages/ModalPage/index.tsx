a991f98e9a65fa5150b6a8d883d7fb1193b2eb8f
ooherin <555ohr@naver.com>
2024-01-23 22:56:15 +0900

    Fix: 모달 수정 진행중

--- src/pages/ModalPage/index.tsx
+++ src/pages/ModalPage/index.tsx
@@ -9,20 +9,20 @@ import BasicModal from '@/components/BasicModal.tsx';
 
 const ModalPage = () => {
   const modalProps1 = MODAL.CONFIRM_BATTLE_JOIN;
-  //const modalProps2 = MODAL.CONFIRM_BATTLE_JOIN;
-  // const modalProps3 = MODAL.CONFIRM_BATTLE_JOIN;
-  // const modalProps4 = MODAL.CONFIRM_BATTLE_JOIN;
-  // const modalProps5 = MODAL.CONFIRM_BATTLE_JOIN;
+  const modalProps2 = MODAL.CONFIRM_BATTLE_JOIN;
+  const modalProps3 = MODAL.CONFIRM_BATTLE_JOIN;
+  const modalProps4 = MODAL.CONFIRM_BATTLE_JOIN;
+  const modalProps5 = MODAL.CONFIRM_BATTLE_JOIN;
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
         <Button variant="outline">버튼</Button>
       </AlertDialogTrigger>
       <BasicModal {...modalProps1} />
-      {/*<BasicModal {...modalProps2} />*/}
-      {/*<BasicModal {...modalProps3} />*/}
-      {/*<BasicModal {...modalProps4} />*/}
-      {/*<BasicModal {...modalProps5} />*/}
+      <BasicModal {...modalProps2} />
+      <BasicModal {...modalProps3} />
+      <BasicModal {...modalProps4} />
+      <BasicModal {...modalProps5} />
       {/*<BasicModal*/}
       {/*  title={MODAL.DELETE_COMMENT.title}*/}
       {/*  content={MODAL.DELETE_COMMENT.content}*/}
