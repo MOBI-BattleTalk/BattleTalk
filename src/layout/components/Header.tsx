d8c839753116a6ed92a7bf8001eb87b1861eb700
ooherin <555ohr@naver.com>
2024-01-25 22:12:47 +0900

    Fix : 로고 수정 및 적용

--- src/layout/components/Header.tsx
+++ src/layout/components/Header.tsx
@@ -2,6 +2,9 @@ import ProfileButton from '@/layout/components/ProfileButton.tsx';
 import LoginButton from '@/layout/components/LoginButton.tsx';
 import { useNavigate } from 'react-router-dom';
 import SessionStorage from '@/utils/sessionStorage.tsx';
+import HeaderLogo from '../../../public/HeaderLogo.png';
+import HeaderLogoAction from '../../../public/HeaderLogoAction.png';
+import { useState } from 'react';
 
 interface Props {
   isLogin: boolean;
@@ -13,6 +16,8 @@ const Header: React.FC<Props> = ({ isLogin }) => {
   const nickName = SessionStorage.getItem('nickName');
   const profileUrl = SessionStorage.getItem('profileUrl');
 
+  const [isHover, setIsHover] = useState(false);
+
   return (
     <div className="w-full flex items-center h-[80px] justify-between bg-white bg-opacity-50 fixed top-0">
       <div
@@ -22,8 +27,11 @@ const Header: React.FC<Props> = ({ isLogin }) => {
         }}
       >
         <img
-          className="w-[200px] pl-[20px]"
-          src="../../../public/Logo.png"
+          onMouseOver={() => setIsHover(true)}
+          onMouseOut={() => setIsHover(false)}
+          onClick={() => navigate('/')}
+          src={isHover ? HeaderLogoAction : HeaderLogo}
+          className={'scale-150 ml-[80px]'}
           alt="logo"
         />
       </div>
