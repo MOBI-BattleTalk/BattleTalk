79fa50e40aaa6432edae73a23df265489a0bc35c
ooherin <555ohr@naver.com>
2024-01-27 01:19:53 +0900

    Feat : 메인 페이지 UI 데이터 연결

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,9 +1,12 @@
 import MainBattleCard from '@/pages/MainPage/components/MainBattleCard.tsx';
+import { BattleMockList } from '@/mock/postMock.ts';
 
 const MainPage = () => {
   return (
     <div className={'flex gap-[30px] flex-col pb-[50px]'}>
-      <MainBattleCard />
+      {BattleMockList.map((post) => {
+        return <MainBattleCard post={post} />;
+      })}
     </div>
   );
 };
