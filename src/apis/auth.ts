b19806312150cc83262b336d25d7b66759020963
ooherin <555ohr@naver.com>
2024-01-27 00:19:36 +0900

    Feat : 응답 인터셉터 구현 완료

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -20,71 +20,47 @@ type SignInDataType = {
 const AuthApi = {
   //로그인
   postSignIn: async (data: SignInType) => {
-    try {
-      const res: AxiosResponse<SignInDataType, Error> =
-        await axiosInstance.post(END_POINTS.USER_SIGN_IN, data);
-      //토큰 저장
-      cookieStorage.setCookie(ACCESS_TOKEN, res.data.token, 60);
-      //유저의 정보 저장
-      LocalStorage.setItem(
-        STORAGE_KEYS.USER_INFO,
-        JSON.stringify({
-          userId: res.data.userId,
-          nickName: res.data.info.nickName,
-          profileUrl: res.data?.info?.profileUrl,
-        }),
-      );
-      if (res.status === 200) {
-        return res;
-      }
-      //리프레시 요청 로직
-      if (res.status === 401) {
-        await AuthApi.postRefresh();
-      }
-      if (res.status === 403) {
-      }
-      return res.data;
-    } catch (err) {
-      console.log(err);
-    }
+    const res: AxiosResponse<SignInDataType, Error> = await axiosInstance.post(
+      END_POINTS.USER_SIGN_IN,
+      data,
+    );
+    //토큰 저장
+    cookieStorage.setCookie(ACCESS_TOKEN, res.data.token, 60);
+    //유저의 정보 저장
+    LocalStorage.setItem(
+      STORAGE_KEYS.USER_INFO,
+      JSON.stringify({
+        userId: res.data.userId,
+        nickName: res.data.info.nickName,
+        profileUrl: res.data?.info?.profileUrl,
+      }),
+    );
   },
   //회원가입
   postSignUp: async (data: SignUpType) => {
     const { userId, password, nickname } = data;
-    try {
-      const res = await axiosInstance.post(END_POINTS.USER_SIGN_UP, {
-        userId: userId,
-        password: password,
-        data: {
-          nickName: nickname,
-        },
-      });
-      return res.data;
-    } catch (err) {
-      console.log(err);
-    }
+    const res = await axiosInstance.post(END_POINTS.USER_SIGN_UP, {
+      userId: userId,
+      password: password,
+      data: {
+        nickName: nickname,
+      },
+    });
+    return res.data;
   },
   //로그아웃
   postSignOut: async () => {
-    try {
-      const res = await axiosInstance.post(END_POINTS.USER_SIGN_OUT);
-      //저장된 토큰 삭제
-      cookieStorage.deleteCookie('accessToken');
-      //저장된 유저 정보 삭제
-      LocalStorage.removeItem('userInfo');
-      return res.data;
-    } catch (err) {
-      console.log(err);
-    }
+    const res = await axiosInstance.post(END_POINTS.USER_SIGN_OUT);
+    //저장된 토큰 삭제
+    cookieStorage.deleteCookie(ACCESS_TOKEN);
+    //저장된 유저 정보 삭제
+    LocalStorage.removeItem('userInfo');
+    return res.data;
   },
   //리프레시 토큰 발급
   postRefresh: async () => {
-    try {
-      const res = await axiosInstance.get(END_POINTS.USER_REFRESH);
-      console.log(res);
-    } catch (err) {
-      console.log(err);
-    }
+    const res = await axiosInstance.get(END_POINTS.USER_REFRESH);
+    return res.data.token;
   },
 };
 
--- src/apis/core.ts
+++ src/apis/core.ts
@@ -17,30 +17,57 @@ export const axiosInstance = axios.create({
   },
   withCredentials: true,
 });
-axiosInstance.interceptors.request.use(
+
+/**
+ * 응답 인터셉터
+ * 응답을 보내기전 intercept하여 해당 로직을 실행시킵니다.
+ *
+ * 401 : accessToken이 유효하지 않다는 것으로 리프레시 토큰을 발급받아
+ *      새로운 토큰으로 재요청을 보냅니다.
+ * 403 : accessToken, refreshToken이 모두 유효하지 않을 때
+ *      다시 로그인을 하도록 로그인 페이지로 direct, 로그아웃 api 요청
+ */
+let isRefreshing = false;
+
+axiosInstance.interceptors.response.use(
   (res) => {
     return res;
   },
   async (err) => {
     //토큰이 유효하지 않을 시에 리프레시 발급 요청
-    if (err.response.status === 401) {
-      await AuthApi.postRefresh();
-      CookieStorage.deleteCookie(ACCESS_TOKEN);
-      //토큰이 리프레시 토큰을 재발급 받으면 자동으로 재요청 로직이 실행되는 것일까요?
-    }
     const originalRequest = err.config;
+
+    if (err.response.status === 401) {
+      if (!isRefreshing) {
+        isRefreshing = true;
+
+        //리프레시 요청을 보내고 성공시 재요청, 실패시 로그아웃
+        try {
+          const token = await AuthApi.postRefresh();
+          CookieStorage.setCookie(ACCESS_TOKEN, token, 60 * 24);
+
+          //발급 받은 토큰으로 요청에 토큰 수정
+          originalRequest.headers.common['Authorization'] = `Bearer ${token}`;
+          //axiosInstace의 common에도 토큰 수정
+          axiosInstance.defaults.headers.common['Authorization'] =
+            `Bearer ${token}`;
+          return axiosInstance(originalRequest);
+          //리프레시 요청이 실패했을 때 로그아웃
+        } catch (refreshErr) {
+          window.location.href = END_POINTS.LOGIN;
+          await AuthApi.postSignOut();
+          //리프레시 요청을 true 상태로 바꾸어 한번만 실행되게 함.
+        } finally {
+          isRefreshing = true;
+        }
+      }
+    }
+
     if (err.response.status === 403) {
       window.location.href = END_POINTS.LOGIN;
+      await AuthApi.postSignOut();
     }
-    //재요청 전송
-    return axiosInstance(originalRequest);
+
+    return Promise.reject(err);
   },
 );
-
-export const checkAndSetToken = () => {
-  const accessToken = cookieStorage.getCookie(ACCESS_TOKEN);
-  if (!accessToken) {
-    console.log('토큰이 존재하지 않습니다.');
-    window.location.href = END_POINTS.HOME;
-  }
-};
